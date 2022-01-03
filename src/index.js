fe222c3c63640d7fb8ffd5da0e1f888cde36b061
Paul Smith <paulsmithkc@gmail.com>
2022-01-03 14:41:29 -0600

    service worker

--- src/index.js
+++ src/index.js
@@ -14,6 +14,24 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// check if browser supports service workers
+if (navigator.serviceWorker) {
+  // wait for the page to load
+  window.addEventListener('load', () => {
+    if (process.env.NODE_ENV === 'production') {
+      // register service worker in prod environment
+      navigator.serviceWorker.register(
+        `${process.env.PUBLIC_URL}/service-worker.js`
+      );
+    } else {
+      // unregister service worker in dev environment
+      navigator.serviceWorker.ready
+        .then((registration) => registration.unregister())
+        .catch((error) => console.error(error.message));
+    }
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
